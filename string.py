53f139c0ae4152915426a54c06f79e9c81da1af0
kobe020824 <84032962+kobe020824@users.noreply.github.com>
2024-05-22 00:32:51 +0900

    Update string.py

--- string.py
+++ string.py
@@ -10,3 +10,25 @@ for i in my_string :
 for i in my_string:
      x = x - 1
      print(my_string[0:x])
+
+
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            steps {
+                echo 'building the application...'
+            }
+        }
+        stage('test') {
+            steps {
+                echo 'testing the application...'
+            }
+        }
+        stage('deploy') {
+            steps {
+                echo 'deploying the application...'
+            }
+        }
+    }
+}
